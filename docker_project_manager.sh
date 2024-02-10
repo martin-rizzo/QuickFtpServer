155da0a7ce16155c3504163ab12255152d9e89dd
Martin Rizzo <martinrizzo@gmail.com>
2024-02-09 21:23:53 -0300

    refactor: Enhanced docker-command.sh script with clearer structure and added helper functions.
    The 'docker-command.sh' script, responsible for managing the Docker
    image and container for this project, underwent significant improvements.
    The code's internal organization was revamped, introducing helper functions
    and detailed documentation was incorporated into specific functions,
    enhancing readability and facilitating maintenance.

--- docker_project_manager.sh
+++ docker_project_manager.sh
@@ -55,38 +55,47 @@ YELLOW='\e[1;33m'
 BLUE='\e[1;34m'
 DEFAULT_COLOR='\e[0m'
 
+# Displays a warning message
 warning() {
     local message=$1
     echo -e "${YELLOW}WARNING${DEFAULT_COLOR} ${message}"
 }
 
+# Displays an error message
 error() {
     local message=$1
     echo -e "${RED}ERROR:${DEFAULT_COLOR} ${message}"
 }
 
+# Displays a fatal error message and exits the script with status code 1
 fatal_error() {
     local message=$1
     error "$message"
     exit 1
 }
 
-#-------------------------------- COMMANDS ---------------------------------#
-
-
-# Function to list Docker images and containers
-list_docker_info() {
-    echo
-    echo -e '    \e[1;32mDOCKER IMAGES'
-    docker images | awk 'NR==1 {print "    \033[0;30;42m" $0 "\033[0m"} NR>1 {print "    " $0 }'
-    echo
-    echo -e '    \e[1;32mDOCKER CONTAINERS'
-    docker ps -a  | awk 'NR==1 {print "    \033[0;30;42m" $0 "\033[0m"} NR>1 {print "    " $0 }'
-    echo
+# Checks if a Docker image exists in the repository
+docker_image_exists() {
+    docker image inspect "$1" &> /dev/null
 }
 
+# Checks if a Docker container is stopped
+docker_container_stopped() {
+    docker container inspect --format='{{.State.Status}}' "$1" | grep -qi "exited"
+}
 
-# Function to build the container
+# Checks if a Docker container exists
+docker_container_exists() {
+    if [ "$(docker ps -a -q -f name=$1)" ]; then
+        return 0  # Container exists
+    else
+        return 1  # Container does not exist
+    fi
+}
+
+#-------------------------------- COMMANDS ---------------------------------#
+
+# Build the container
 build_image() {
 
     clear_docker_resources
@@ -99,16 +108,8 @@ build_image() {
     echo "Done! The container has been built."
 }
 
-# Stop and remove Docker containers (optionally removes Docker images)
-#
-# Usage: clear_docker_resources [extra_action]
-#
-# Parameters:
-#   [extra_action] - Specify "remove-image" to also remove the
-#                    Docker image associated with the container.
-#
+# Stop and remove container and image
 clear_docker_resources() {
-    local extra_action=$1
 
     # stop the container if it's running
     if [[ "$(docker ps -q -f name=$CONTAINER_NAME)" ]]; then
@@ -123,7 +124,7 @@ clear_docker_resources() {
     fi
 
     # remove the image if it exists
-    if [[ $extra_action == 'remove-image' && "$(docker images -q $IMAGE_NAME)" ]]; then
+    if [[ "$(docker images -q $IMAGE_NAME)" ]]; then
         echo "Removing the existing image..."
         docker rmi $IMAGE_NAME
     fi
@@ -131,22 +132,35 @@ clear_docker_resources() {
     echo "Done! Docker resources cleared."
 }
 
+# Run the container
 run_container() {
-    if ! docker image inspect $IMAGE_NAME &> /dev/null; then
+
+    # if the container already exists
+    # ensure the container is running
+    if docker_container_exists $CONTAINER_NAME ; then
+        if docker_container_stopped $CONTAINER_NAME ; then
+            echo "Starting existing stopped container..."
+            docker start "$CONTAINER_NAME"
+        else
+            echo "Docker container already running"
+        fi
+        return
+    fi
+    
+    # if the container doesn't exist
+    # check for its image; if not found, build it!
+    if ! docker_image_exists $IMAGE_NAME ; then
         build_image
     fi
-    if docker ps -a --filter "name=$CONTAINER_NAME" --format '{{.Status}}' | grep -q 'Exited'; then
-        echo "Starting existing stopped container..."
-        docker start "$CONTAINER_NAME"
-        return 0
-    else
-        echo "Starting new container..."
-        echo '>' docker run $CONTAINER_PARAMETERS --name "$CONTAINER_NAME" "$IMAGE_NAME"
-        docker run $CONTAINER_PARAMETERS --name "$CONTAINER_NAME" "$IMAGE_NAME"
-    fi    
+    
+    # start a new container with specified parameters
+    echo "Starting new container..."
+    echo '>' docker run $CONTAINER_PARAMETERS --name "$CONTAINER_NAME" "$IMAGE_NAME"
+    docker run $CONTAINER_PARAMETERS --name "$CONTAINER_NAME" "$IMAGE_NAME"
+
 }
 
-# stop the container if it's running
+# Stop the container if it's running
 stop_container() {
     if [[ "$(docker ps -q -f name=$CONTAINER_NAME)" ]]; then
         echo "Stopping the existing container..."
@@ -158,6 +172,21 @@ restart_container() {
     stop_container && run_container
 }
 
+open_console_in_container() {
+    docker exec -it "$CONTAINER_NAME" /bin/sh
+}
+
+# List images and containers
+list_docker_info() {
+    echo
+    echo -e '    \e[1;32mDOCKER IMAGES'
+    docker images | awk 'NR==1 {print "    \033[0;30;42m" $0 "\033[0m"} NR>1 {print "    " $0 }'
+    echo
+    echo -e '    \e[1;32mDOCKER CONTAINERS'
+    docker ps -a  | awk 'NR==1 {print "    \033[0;30;42m" $0 "\033[0m"} NR>1 {print "    " $0 }'
+    echo
+}
+
 show_container_logs() {
     if ! docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
         echo "Error: The container $CONTAINER_NAME does not exist." >&2
@@ -167,20 +196,21 @@ show_container_logs() {
     docker logs $CONTAINER_NAME
 }
 
-open_console_in_container() {
-    docker exec -it "$CONTAINER_NAME" /bin/sh
+execute_command_in_container() {
+    fatal_error "Not implemented"
+}
+
+show_container_status() {
+    fatal_error "Not implemented"
 }
 
 # Main script logic
 case "$1" in
-    "list")
-        list_docker_info
-        ;;
-    "clean")
-        clear_docker_resources remove-image
-        ;;
     "build")
         build_image
+        ;;
+    "clean")
+        clear_docker_resources
         ;;
     "run")
         run_container
@@ -194,34 +224,19 @@ case "$1" in
     "console")
         open_console_in_container
         ;;
-#    "logs")
-#        show_container_logs
-#        ;;
-#    "status")
-#        show_container_status
-#        ;;
-#    "push")
-#        push_to_registry
-#        ;;
-#    "pull")
-#        pull_from_registry
-#        ;;
-#    "exec")
-#        execute_command_in_container
-#        ;;
-#    "config")
-#        view_edit_container_config
-#        ;;
-#    "backup")
-#        backup_container_data
-#        ;;
-#    "restore")
-#        restore_container_data
-#        ;;
-#    "inspect")
-#        inspect_container
-#        ;;
+    "list")
+        list_docker_info
+        ;;
+   "logs")
+       show_container_logs
+       ;;
+   "exec")
+       execute_command_in_container
+       ;;
+   "status")
+       show_container_status
+       ;;
     *)
-        build_and_run_ftp_server
+        build_image
         ;;
 esac
